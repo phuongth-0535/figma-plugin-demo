691780a45f74950ad1751aa39b63bbf6f931acb8
Son Dao Thai <dtson96@gmail.com>
2024-03-20 09:29:03 +0700

    chore(ci): upload screenshot to artifact

---
+++ screenshot.js
@@ -0,0 +1,9 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:8080');
+  await page.screenshot({path: '01_screenshot.png'});
+  await browser.close();
+})();
